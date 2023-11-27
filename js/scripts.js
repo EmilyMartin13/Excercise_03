60c6c384db2857fe66a4f00ce739ae92c7456b1e
EmilyMartin13 <em.martin@uleth.ca>
2023-11-26 22:30:03 -0700

    NOV 26 10:30PM

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,52 @@ console.log(x)
 
 $('#Happy').on('click', function () {
     $('#HappyPIC').show();
+    
 });
 
+$('#Sad').on('click', function () {
+    $('#SadPIC').show();
+});
 
+$('#Angry').on('click', function () {
+    $('#AngryPIC').show();
+});
 
+$('#Shocked').on('click', function () {
+    $('#ShockedPIC').show();
+});
 
+$('#Confused').on('click', function () {
+    $('#ConfusedPIC').show();
+});
 
+$("#Sad', #Angry, #Confused, #Shocked").on('click', function () {
+    $('#HappyPIC').hide();
+});
 
+$("#Happy, #Angry, #Confused, #Shocked").on('click', function () {
+    $('#SadPIC').hide();
+});
 
+$("#Happy, #Sad, #Confused, #Shocked").on('click', function () {
+    $('#AngryPIC').hide();
+});
 
+$("#Happy, #Sad, #Angry, #Shocked").on('click', function () {
+    $('#ConfusedPIC').hide();
+});
+
+$("#Happy, #Sad, #Angry, #Confused").on('click', function () {
+    $('#ShockedPIC').empty();
+});
+
+ var hideImage = true;
+ $(".HappyPIC").on("click", function () {
+     if ("#Sad, #Angry, #Confused, #Shocked, click" == true){
+         $('#HappyPIC').hide();
+         hideImage = true;
+     } else {
+         $(this).show();
+         hideImage = false;
+     }
+ });
